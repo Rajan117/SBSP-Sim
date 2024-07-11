3e3b49a0805e863c15c0e8addd461fcb22962325
Rajan117 <77441871+Rajan117@users.noreply.github.com>
2024-07-11 10:58:37 +0100

    Created FStruct for Sim Settings
    This data structure will hold the different properties regarding the simulation for better encapsulation and cleaner code.

--- SBSP/Source/SBSP/Controllers/SBSPPlayerController.h
+++ SBSP/Source/SBSP/Controllers/SBSPPlayerController.h
@@ -9,6 +9,23 @@
 class ASpaceStructure;
 class ASBSPHUD;
 
+USTRUCT()
+struct FSimSettings
+{
+	GENERATED_BODY()
+	//Space Structure Properties
+	int32 StructureRadius = 2;
+	//Harbour Properties
+	int32 InitStock = 50;
+	int32 TileRadius = 4;
+	int32 NumRobots = 5;
+	//RobotProperties
+	float RobotSpeed = 1.f;
+	//RocketProperties
+	float LaunchFrequency = 10.f;
+	int32 TilePayload = 10;
+};
+
 UCLASS()
 class SBSP_API ASBSPPlayerController : public APlayerController
 {
